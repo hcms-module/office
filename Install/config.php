149d533f2af1d1a1b660e052a041dc9e547d7b0b
zhlhuang <364626853@qq.com>
2023-02-09 12:02:29 +0800

    升级兼容 Hcms3.0

--- Install/config.php
+++ Install/config.php
@@ -5,12 +5,12 @@ declare(strict_types=1);
 return [
     'name' => 'Office',
     'require' => [
-        'hcms_version' => '0.8.0',
+        'hcms_version' => '3.0.0',
         'composer' => [
             'phpoffice/phpspreadsheet' => '^1.22',
             'phpoffice/phpword' => '^0.18.3',
         ],
         'module' => []
     ],
-    'version' => '1.3.0'
+    'version' => '3.0.0'
 ];
